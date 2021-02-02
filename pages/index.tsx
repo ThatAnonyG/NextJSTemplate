c84725ebf5c4934aa0861dfc48a4bfaa5269c34a
Ratul Saha <0anonymous0guy0@gmail.com>
2021-02-02 10:10:50 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Home(): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>Merakism Project</title>
+			</Head>
+			<h1>Home</h1>
+		</>
+	);
+}
+
+export default Home;
